069fedba3d65dbc8e5af8c82b7f164f2eeb84f53
Andrew John Hack <98097894+40457712@users.noreply.github.com>
2023-09-11 15:08:12 +0100

    Update

--- src/components/CryptoList.js
+++ src/components/CryptoList.js
@@ -1,6 +1,22 @@
+import React from 'react';
+
+import Crypto from './Crypto';
+// import classes from './MoviesList.module.css';
+
 const CryptoList = (props) =>{
-    consolelog("CryptoList");
-    consolelog({props});
+    console.log("CryptoList");
+    console.log({props});
+    return (
+    //    <ul className={classes['movies-list']}>
+        <ul>
+          {props.cryptos.map((crypto) => (
+            <Crypto
+              time={crypto.time}
+              volume={crypto.volume}
+            />
+          ))}
+        </ul>
+      );
 };
 
 export default CryptoList;
