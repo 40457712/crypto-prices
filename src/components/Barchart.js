032c937d0f3690b09f92082f82570a54e62d8e48
Andrew John Hack <98097894+40457712@users.noreply.github.com>
2023-09-12 10:42:35 +0100

    Update

--- src/components/Barchart.js
+++ src/components/Barchart.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
 //import classes from './Movie.module.css';
+const Chart = ({ children, width, height }) => (
+    <svg viewBox={`0 0 ${width} ${height}`} width={width} height={height}>
+      {children}
+    </svg>
+  )
+  
+  const Bar = ({ x, y, width, height }) => (
+    <rect x={x} y={y} width={width} height={height} />
+  )
 
-const Barchart = (props) => {
+const Barchart = (data) => {
 
-    const Chart = ({ children, width, height }) => (
-        <svg viewBox={`0 0 ${width} ${height}`} width={width} height={height}>
-          {children}
-        </svg>
-      )
-      
-      const Bar = ({ x, y, width, height }) => (
-        <rect x={x} y={y} width={width} height={height} />
-      )
-
-      const BarChart = ({ data }) => {
         // Width of each bar
         const itemWidth = 20
       
@@ -48,7 +46,6 @@ const Barchart = (props) => {
             ))}
           </Chart>
         )
-      }
-};
+      };
 
 export default Barchart;
